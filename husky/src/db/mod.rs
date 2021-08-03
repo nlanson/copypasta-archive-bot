4376104888c2a9de61b94f02420b5537dbc384a9
Noah Lanson <lanson.noah@gmail.com>
2021-08-03 16:37:52 +1000

    Trying better SQL checkers

--- husky/src/db/mod.rs
+++ husky/src/db/mod.rs
@@ -26,24 +26,35 @@ impl Database {
     //Adds a new pasta with key name and value content.
     pub fn add(&self, name: &str, content: &str) -> Result<(), PastaErr> {
         let connection = sqlite::open(&self.file)?;
+        //Trying better methods.
+        //Not working yet.
+        let statement = format!(
+            "
+                select * from pastas where name={}
+                where not exists(insert into pastas (name, value) values ({}, {}));
+            ", name, name, content
+        );
+        connection.prepare(statement)?;
+        Ok(())
+        
 
         //Inserts the params into the database.
         //Check for duplicates by getting the name from the database first.
-        match Self::get(&self, name) {
-            //If get succeeds, then there is a duplicate.
-            //Return a user error
-            Ok(_) => Err(PastaErr::UsrErr(String::from("duplicate pasta"))),
+        // match Self::get(&self, name) {
+        //     //If get succeeds, then there is a duplicate.
+        //     //Return a user error
+        //     Ok(_) => Err(PastaErr::UsrErr(String::from("duplicate pasta"))),
 
-            //If the get fails, then there is most likely no duplicate.
-            //Add the pasta into the database and return Ok.
-            Err(_) => {
-                let mut db = connection.prepare("insert into pastas (name, value) values (?, ?);")?;
-                db.bind(1, name)?;
-                db.bind(2, content)?;
-                db.next()?;
-                Ok(())
-            }
-        }
+        //     //If the get fails, then there is most likely no duplicate.
+        //     //Add the pasta into the database and return Ok.
+        //     Err(_) => {
+        //         let mut db = connection.prepare("insert into pastas (name, value) values (?, ?);")?;
+        //         db.bind(1, name)?;
+        //         db.bind(2, content)?;
+        //         db.next()?;
+        //         Ok(())
+        //     }
+        // }
     }
 
     //Returns the pasta with key name.
