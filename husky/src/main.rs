85ddeaa2e89f8f8a9dab3f80c5d406e7f67f895b
Noah Lanson <lanson.noah@gmail.com>
2021-08-05 10:56:33 +1000

    Disabled rocket logging for simpler log

--- husky/src/main.rs
+++ husky/src/main.rs
@@ -1,67 +1,94 @@
+//Dependencies and Modules
 #[macro_use] extern crate rocket;
-pub mod db;
+use serde::{Serialize, Deserialize};
+mod db;
+mod log;
+use log::{
+    Level as lvl,
+    print as log
+};
+
 /*
     Todo:
         - Return data in JSON format with success status etc. (See jSend specification)
 */
 
-
-//Save copypastas here.
-#[get("/<name>/<value>")]
-fn save(name: &str, value: &str) -> String {
-    let db = db::Database::new(String::from("./pastas.db"));
-    
-    //Add pasta
-    let flag: bool;
-    match db.add(name, value) {
-        //Successful
-        Ok(_) => {
-            println!("Successfully added new pasta '{}'", name);
-            flag = true;
-        },
-
-        //Database SQLITE error
-        Err(db::PastaErr::DbErr(ref err)) => {
-            println!("Sqlite Error Code: {:?} | Message: {:?}", err.code, err.message);
-            flag = false;
-        }
-    }
-    
-
-    if flag {
-        format!("Successful")
-    } else {
-        format!("Unsuccessful")
-    }
-    
-}
-
-//Get copypastas here.
-#[get("/<name>")]
-fn send(name: &str) -> String {
-    let db = db::Database::new(String::from("./pastas.db"));
-
-    //Get pasta
-    let val: String;
-    match db.get(name) {
-        //Success
-        Ok(pasta) => val = pasta,
-
-        //Database SQLITE error
-        Err(db::PastaErr::DbErr(ref err)) => {
-            println!("Sqlite Error Code: {:?} | Message: {:?}", err.code, err.message);
-            val = String::from("sqlite error")
-        }
-    }
-    
-    //Should return an error status code if val is sqlite error or user error.
-    format!("{}", val)
-}
-
-
+//Start server
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/save", routes![save])
         .mount("/send", routes![send])
+}
+
+
+//Save copypastas here
+#[get("/<name>/<value>")]
+fn save(name: &str, value: &str) -> String {
+    log(lvl::Info, &format!("Save Requested | '{}'", name));
+    
+    //Add pasta to db
+    let db = db::Database::new(String::from("./pastas.db"));
+    let res: Response;
+    match db.add(name, value) {
+        //Successful
+        Ok(_) => {
+            log(lvl::Info, "Success");
+            res = Response::new(String::from("success"), None);
+        },
+
+        //Database SQLITE error
+        Err(db::PastaErr::DbErr(ref err)) => {
+            log(lvl::Error, &format!("Failed | {:?}", err.message));
+            res = Response::new(String::from("fail"), None);
+        }
+    }
+
+    //Return json string
+    res.to_json()
+}
+
+//Get copypastas here
+#[get("/<name>")]
+fn send(name: &str) -> String {
+    log(lvl::Info, &format!("Send Requested | '{}'", name));
+    
+    //Get pasta and send result.
+    let db = db::Database::new(String::from("./pastas.db"));
+    let res: Response;
+    match db.get(name) {
+        //Success
+        Ok(pasta) => {
+            log(lvl::Info, "Success");
+            res = Response::new(String::from("success"), Some(pasta));
+        },
+
+        //Database SQLITE error
+        Err(db::PastaErr::DbErr(ref err)) => {
+            log(lvl::Error, &format!("Failed | {:?}", err.message));
+            res = Response::new(String::from("fail"), None);
+        }
+    }
+    
+    //Return json string
+    res.to_json()
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    status: String,
+    data: Option<String>
+}
+
+impl Response {
+    pub fn new(status: String, data: Option<String>) -> Self {
+        Self {
+            status,
+            data
+        }
+    }
+    
+    pub fn to_json(&self) -> String{
+        serde_json::to_string(&self).unwrap()
+    }
 }
