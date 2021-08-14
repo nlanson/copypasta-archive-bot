1f111c6e4b0b28a90fabcebf4f3b0292a65be261
Noah Lanson <lanson.noah@gmail.com>
2021-08-14 21:21:22 +1000

    Ready to deploy to copypasta

--- shepard/src/bot.ts
+++ shepard/src/bot.ts
@@ -22,7 +22,7 @@ export class Bot {
         }
         log(Level.Info, "Bot connected.");
         this.connectedAt = Date.now() / 1000;
-        this.streamComments('u_keijyu'); //Set subreddit to stream comments
+        this.streamComments('copypasta'); //Set subreddit to stream comments
     }
 
     private loadBotCredentials(): SnoowrapOptions {
